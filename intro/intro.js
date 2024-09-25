615bdd4192df4b2bd24c3e39ad3d275c9f856b60
Apurva <117301920+apurvak3@users.noreply.github.com>
2024-09-25 17:21:29 +0530

    added intro

---
+++ intro/intro.js
@@ -0,0 +1,3 @@
+var greetings = "hello world";
+console.log(greetings);
+export{};
---
+++ intro/intro.ts
@@ -0,0 +1,6 @@
+let greetings : string = "hello world" ;
+greetings.toLowerCase();
+let userID = 22001013022;
+userID.toFixed();
+let isloggedIn :boolean = false;
+console.log(greetings);
